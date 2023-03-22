63a302aad4d8c6a788bcf3253993c5c5fe18be4a
Roman1917 <wovkroma1@gmail.com>
2023-03-22 22:29:25 +0200

    deploy

--- src/Api/Api.ts
+++ src/Api/Api.ts
@@ -10,54 +10,66 @@ const API: any = {
     type: string;
     numberInList: number;
   }) => {
-    return fetch(`${localHost}/todo/todo/addtask`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(payload),
-    });
+    return fetch(
+      `https://backend-helpua-app.herokuapp.com//todo/todo/addtask`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload),
+      }
+    );
   },
   fetchTodo: () => {
-    return fetch(`${localHost}/todo/todo/fetchtodo`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin":
-          " http://localhost:4000/todo/todo/fetchtodo",
-      },
-    });
+    return fetch(
+      `https://backend-helpua-app.herokuapp.com//todo/todo/fetchtodo`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin":
+            " http://localhost:4000/todo/todo/fetchtodo",
+        },
+      }
+    );
   },
   toggleTodo: (payload: {
     id: string;
     status: boolean;
     numberTodo: number;
   }) => {
-    return fetch(`${localHost}/todo/todo/toggletodo/:${payload.id}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": `http://localhost:4000/todo/todo/toggletodo/${payload.id}`,
-      },
-      body: JSON.stringify({
-        id: payload.id,
-        status: payload.status,
-        numberTodo: payload.numberTodo,
-      }),
-    });
+    return fetch(
+      `https://backend-helpua-app.herokuapp.com//todo/todo/toggletodo/:${payload.id}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": `http://localhost:4000/todo/todo/toggletodo/${payload.id}`,
+        },
+        body: JSON.stringify({
+          id: payload.id,
+          status: payload.status,
+          numberTodo: payload.numberTodo,
+        }),
+      }
+    );
   },
   showModal: (payload: { id: string; modalStatus: boolean }) => {
-    return fetch(`${localHost}/todo/todo/showmodaltodo/:${payload.id}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": `http://localhost:4000/todo/todo/showmodaltodo/${payload.id}`,
-      },
-      body: JSON.stringify({
-        id: payload.id,
-        modalStatus: payload.modalStatus,
-      }),
-    });
+    return fetch(
+      `https://backend-helpua-app.herokuapp.com//todo/todo/showmodaltodo/:${payload.id}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": `http://localhost:4000/todo/todo/showmodaltodo/${payload.id}`,
+        },
+        body: JSON.stringify({
+          id: payload.id,
+          modalStatus: payload.modalStatus,
+        }),
+      }
+    );
   },
 };
 
